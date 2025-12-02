c19a6dbec440cdb090cc2b0d81892183edeafb87
barathkumar <146552073+barathkumar229@users.noreply.github.com>
2025-12-02 23:29:39 +0530

    Set base path for Vite configuration
    Updated base path in Vite configuration for repository.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// GitHub repo name
+const repoName = "portfolios";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`, // <-- updated for your repo
   server: {
     host: "::",
     port: 8080,
